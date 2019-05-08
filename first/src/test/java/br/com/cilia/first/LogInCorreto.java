50aaf531d35d16f0467f877ab82c8137750a2c6e
paularesende <bellsuo@gmail.com>
2019-05-08 05:08:32 -0300

    Preenchimento da senha e verificação de assertividade; fim da primeira user story

--- first/src/test/java/br/com/cilia/first/LogInCorreto.java
+++ first/src/test/java/br/com/cilia/first/LogInCorreto.java
@@ -1,5 +1,7 @@
 package br.com.cilia.first;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 
@@ -27,10 +30,23 @@ public class LogInCorreto {
 	    }
 	    
 	    @Test //passa elementos como parâmetros para preenchimento dos campos encontrados no navegador
-	    public void preencherEmailSenha()
+	    public void preencherEmailSenha() throws InterruptedException
 	    {	    	
 	     WebElement email = driver.findElement(By.name("username"));
 	     email.sendKeys("user@phptravels.com");
+	     
+	     //thread fornece tempo para que a visualização de preenchimento dos campos seja possibilitada
+	     Thread.sleep(2000);
+	     WebElement senha = driver.findElement(By.name("password"));
+	     senha.sendKeys("demouser");
+	     Thread.sleep(2000);
+	     
+	     //encontra o botão de login e clica
+	     driver.findElement(By.cssSelector("button.btn.btn-action.btn-lg.btn-block.loginbtn")).click();
+	     Thread.sleep(50000);
+	     
+	     //verifica se o login foi realizado de maneira devida
+	     assertEquals(driver.getCurrentUrl(), "https://www.phptravels.net/account/");	     
 	    }
 
 	   
